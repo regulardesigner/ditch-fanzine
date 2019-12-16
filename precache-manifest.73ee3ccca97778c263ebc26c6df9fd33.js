25d88af42a7768d14880cd312fc9c2989c916834
Damien Tailhades <damien.tailhades@gmail.com>
2019-12-16 14:57:25 +0100

    Updates

---
+++ precache-manifest.73ee3ccca97778c263ebc26c6df9fd33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c73b5ccc61937c2dcfdc4ce0739265",
+    "url": "/ditch-fanzine/index.html"
+  },
+  {
+    "revision": "242d549286791bc0d637",
+    "url": "/ditch-fanzine/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1f6bebe61a66febf1e89",
+    "url": "/ditch-fanzine/static/js/2.eafb831c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ditch-fanzine/static/js/2.eafb831c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "242d549286791bc0d637",
+    "url": "/ditch-fanzine/static/js/main.9969eac0.chunk.js"
+  },
+  {
+    "revision": "ef1739a428cb4103d959",
+    "url": "/ditch-fanzine/static/js/runtime-main.fdcb7f72.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ditch-fanzine/static/media/logo.5d5d9eef.svg"
+  }
+]);
