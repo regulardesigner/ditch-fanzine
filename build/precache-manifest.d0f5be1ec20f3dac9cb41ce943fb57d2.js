69693bfb0c43974ccac0087f9ba56370c9d40300
Damien Tailhades <damien.tailhades@gmail.com>
2019-12-16 17:39:24 +0100

    first commit - basic homepage

---
+++ build/precache-manifest.d0f5be1ec20f3dac9cb41ce943fb57d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdefc6700d50a81657156a5fce6dd57a",
+    "url": "/ditch-fanzine/index.html"
+  },
+  {
+    "revision": "656cfa02bdd30168989b",
+    "url": "/ditch-fanzine/static/css/main.cbefd63a.chunk.css"
+  },
+  {
+    "revision": "1f6bebe61a66febf1e89",
+    "url": "/ditch-fanzine/static/js/2.eafb831c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ditch-fanzine/static/js/2.eafb831c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "656cfa02bdd30168989b",
+    "url": "/ditch-fanzine/static/js/main.cc583fda.chunk.js"
+  },
+  {
+    "revision": "ef1739a428cb4103d959",
+    "url": "/ditch-fanzine/static/js/runtime-main.fdcb7f72.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Ditch</h1>
+        <p className="app-soon-title">Future et déchets abandonnés</p>
+        <p className="app-soon">#001 [soon]</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
